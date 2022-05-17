c153597ff9df2450e397a384bdf4404405c596fb
LiN <ilazystranger@gmail.com>
2022-05-17 17:17:57 +0300

    g

---
+++ js/index_import.js
@@ -0,0 +1,19 @@
+$.ajaxSetup({ cache: false });
+$.get('https://raw.githubusercontent.com/LazyStrangg/kursavaya/master/xml/data.xml')
+  .done(function(data){
+    // parse the xml
+    data = $.parseXML(data);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+     
+      $(data).find("apartmant").each(
+        function(index, element){
+              let field = $(element)
+              alert(field.find("description").text())
+        }
+    )
+  })
+  .fail(function(){
+    alert('Check internet connection!');
+  })
+;
